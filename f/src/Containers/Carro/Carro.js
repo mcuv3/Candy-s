1d1d18982d42a58e802416a82c154c5e16c79962
MCUVE <MauricioAntonioMartinez@outlook.com>
2020-06-09 11:56:14 -0500

    front

--- f/src/Containers/Carro/Carro.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Carro = (props) => {
-  return <div></div>;
-};
-
-export default Carro;
--- f/src/components/UI/Imagen/Imagen.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import classes from "./Imagen.css";
-
-const Imagen = (props) => {
-  return <img className={classes.Imagen} src={props.url} alt={props.alt} />;
-};
-
-export default Imagen;
